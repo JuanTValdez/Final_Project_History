ff7de84539b82383a652d9c12b19e6dcfa4e011b
JuanValdez <juantvaldez85@gmail.com>
2022-07-11 16:34:04 -0400

    changed card design

--- client/src/App.js
+++ client/src/App.js
@@ -78,7 +78,7 @@ function App() {
         </Stack>
       </LocalizationProvider> */}
       <h3>Example </h3>
-      <div className='card'>
+      {/* <div className='card'>
         <div className='thumbnail'>
           <img src='https://www.buyflags.eu/sites/default/files/styles/medium/public/flags-image/state-flags/canada.png?itok=i7jzXf98' />
         </div>
@@ -87,7 +87,60 @@ function App() {
 
           <p>Category: {categoryName} </p>
         </div>
+      </div> */}
+
+      <div class='container'>
+        <div class='cellphone-container'>
+          <div class='movie'>
+            <div class='movie-img'></div>
+            <div class='text-movie-cont'>
+              <div class='mr-grid'>
+                <div class='col1'>
+                  <h1>Interstellar</h1>
+                  <ul class='movie-gen'>
+                    <li>PG-13 /</li>
+                    <li>2h 49min /</li>
+                    <li>Adventure, Drama, Sci-Fi,</li>
+                  </ul>
+                </div>
+              </div>
+              <div class='mr-grid summary-row'>
+                <div class='col2'>
+                  <h5>SUMMARY</h5>
+                </div>
+                <div class='col2'></div>
+              </div>
+              <div class='mr-grid'>
+                <div class='col1'>
+                  <p class='movie-description'>
+                    A group of elderly people are giving interviews about having
+                    lived in a climate of crop blight and constant dust
+                    reminiscent of The Great Depression of the 1930's. The first
+                    one seen is an elderly woman stating her father was a
+                    farmer, but did not start out that way.{' '}
+                  </p>
+                </div>
+              </div>
+              <div class='mr-grid actors-row'>
+                <div class='col1'>
+                  <p class='movie-actors'>
+                    Matthew McConaughey, Anne Hathaway, Jessica Chastain
+                  </p>
+                </div>
+              </div>
+              <div class='mr-grid action-row'>
+                <div class='col2'></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <a href='https://dribbble.com/geehm' target='_blank'>
+        <img
+          class='dribbble-link'
+          src='https://image.flaticon.com/icons/png/512/124/124037.png'
+        />
+      </a>
     </div>
   );
 }
