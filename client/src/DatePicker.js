42f27a3cc889a2fde302e2b23425a066504922c7
JuanValdez <juantvaldez85@gmail.com>
2022-07-19 16:46:21 -0400

    styled input box

--- client/src/DatePicker.js
+++ client/src/DatePicker.js
@@ -4,16 +4,30 @@ import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { makeStyles } from '@material-ui/core';
 import dateFormat from 'dateformat';
 
+const useStyles = makeStyles({
+  dateWrapper: {
+    borderBottom: '2px solid #333333',
+    margin: '0px 0px 25px 0px',
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '25%',
+    margin: '25px auto',
+    borderRadius: '25px',
+  },
+});
+
 export default function DatePicker(props) {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.dateWrapper}>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Stack spacing={3}>
+        <Stack className={classes.input} spacing={3}>
           <DesktopDatePicker
-            label='Date desktop'
+            // label='Date desktop'
             inputFormat='MM-dd-yyyy'
             value={props.value}
             onChange={props.handleChange}
